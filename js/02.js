36c0ab3a65460fd1c50dc94eeedde09d83bab9cf
erick-anthony-jesus <62515201+erick-anthony-jesus@users.noreply.github.com>
2024-05-02 10:44:17 -0500

    Add files via upload

---
+++ js/02.js
@@ -0,0 +1,20 @@
+// String
+let saludar = "Buenos días";
+let buscar = saludar.toLocaleLowerCase();
+//console.log(buscar);
+
+// Numeros
+let numero1 = 15.2;
+let numero2 = 5;
+let numero3 = numero1 * numero2;
+//console.log(numero3);
+
+
+// Booleanos
+let verdad = true;
+let mentira = false;
+console.log( mentira );
+
+
+
+
